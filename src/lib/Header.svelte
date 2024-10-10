ac510d65ea4922d8296467573fd9c8c69a29219b
AJ <alloizeusjohndebelen1@gmail.com>
2024-10-10 23:02:59 +0800

    initial commit -src changes

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,5 @@
 <script>
     export let y;
-
     export let innerHeight;
 
     export let tabs = [
@@ -10,6 +9,7 @@
         // { name: "Contact", link: "" },
         // {name: '', link: ''},
     ];
+    let isDropdownOpen = false;
 </script>
 
 <header
@@ -32,5 +32,31 @@
                 <p class="duration-200">{tab.name}</p>
             </a>
         {/each}
+        <a href="/contact" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-black text-white">
+            <div
+                class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9 font-afacad">Connect with Me</h4>
+        </a>
+    </div>
+    <div class="sm:hidden relative">
+        <button on:click={() => isDropdownOpen = !isDropdownOpen} class="px-4 py-2 bg-black text-white rounded flex items-center gap-2 hover:text-blue-400 duration-200">
+            <i class="fas fa-bars"></i>
+        </button>
+        {#if isDropdownOpen}
+            <div class="absolute right-0 mt-2 w-48 bg-black border border-gray-200 rounded shadow-lg transition-transform transform origin-top-right duration-200 ease-out scale-100">
+                {#each tabs as tab, index}
+                    <a
+                        href={tab.link}
+                        class="block px-4 py-2 text-white hover:text-blue-400 duration-200"
+                    >
+                        <p class="font-afacad">{tab.name}</p>
+                    </a>
+                {/each}
+                <a href="/contact" class="block px-4 py-2 text-white hover:text-blue-400 duration-200 font-afacad">
+                    Tap me
+                </a>
+            </div>
+        {/if}
     </div>
 </header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,10 @@
                 My <span class="text-red-700 font-bold">favorite tech</span> includes Adobe Softwares, Figma, and Canva.
                 I also like the JavaScript (Svelte, React, and Node) and Python programming language.
             </p>
+            <a href="/contact" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-black text-white">
+                <div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+                <h4 class="relative z-9" style="font-family: 'Afacad Flux', sans-serif;">Tap me &rarr;</h4>
+            </a>
         </div>
 
         <div class="relative shadow-2xl grid place-items-center">
